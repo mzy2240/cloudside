c92eda3ccb41818f054be4513497fa630ffc2c26
Zeyu Mao <mao.mzy@gmail.com>
2022-04-29 19:00:33 -0500

    1. Make sure the dependency requirements actually applied.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import pathlib
+import pkg_resources
 
 PACKAGE_DATA = {
     "cloudside.tests.data": ["*.txt", "*.png", "*.dat", "*.csv"],
@@ -7,29 +9,37 @@ PACKAGE_DATA = {
 
 DESCRIPTION = "cloudside - download, assess, and visualize weather data"
 
-if __name__ == "__main__":
-    setup(
-        name="cloudside",
-        version="0.2",
-        author="Paul Hobson",
-        author_email="pmhobson@gmail.com",
-        url="http://python-metar.sourceforge.net/",
-        description=DESCRIPTION,
-        long_description=DESCRIPTION,
-        package_data=PACKAGE_DATA,
-        download_url="http://sourceforge.net/project/platformdownload.php?group_id=134052",
-        license="BSD 3-Clause",
-        packages=find_packages(exclude=[]),
-        platforms="Python 3.6 and later.",
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Environment :: Console",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3.6",
-            "Topic :: Scientific/Engineering :: Atmospheric Science",
-            "Topic :: Scientific/Engineering :: Visualization",
-        ],
-        entry_points={"console_scripts": ["cloudside=cloudside.cli:main"]},
-    )
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
+setup(
+    name="cloudside",
+    version="0.2",
+    author="Paul Hobson",
+    author_email="pmhobson@gmail.com",
+    url="http://python-metar.sourceforge.net/",
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    package_data=PACKAGE_DATA,
+    download_url="http://sourceforge.net/project/platformdownload.php?group_id=134052",
+    license="BSD 3-Clause",
+    packages=find_packages(exclude=[]),
+    platforms="Python 3.6 and later.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    entry_points={"console_scripts": ["cloudside=cloudside.cli:main"]},
+    install_requires=install_requires
+)
