e5c23bc2c5e352450371b4e1f326772d7d9bd512
Zeyu Mao <mao.mzy@gmail.com>
2022-05-01 22:52:06 -0500

    1. Fix the import issue.

--- cloudside/__init__.py
+++ cloudside/__init__.py
@@ -3,10 +3,11 @@ from . import storms
 from . import viz
 from . import ncdc
 from . import asos
+from . import iem
 
 from .tests import test, teststrict
 
 
-__author__ = "Paul Hobson"
-__email__ = "pmhobson@gmail.com"
-__version__ = "0.0.1"
+__author__ = "Zeyu MAO"
+__email__ = "zeyumao2@tamu.edu"
+__version__ = "0.3.0"
